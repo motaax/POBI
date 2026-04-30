7dcd57ad3afa70fbdb5be4929c95ba3c9285b94c
Felipe Mota <felipemota465@gmail.com>
2026-04-30 11:27:22 -0300

    Aula 4
    Aula 4

---
+++ Aula 4/telefone-pj.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for (char c : s) {
+        if (c == '-') {
+            cout << '-';
+        }
+        else if (c >= 'A' && c <= 'C') cout << '2';
+        else if (c >= 'D' && c <= 'F') cout << '3';
+        else if (c >= 'G' && c <= 'I') cout << '4';
+        else if (c >= 'J' && c <= 'L') cout << '5';
+        else if (c >= 'M' && c <= 'O') cout << '6';
+        else if (c >= 'P' && c <= 'S') cout << '7';
+        else if (c >= 'T' && c <= 'V') cout << '8';
+        else if (c >= 'W' && c <= 'Z') cout << '9';
+    }
+
+    cout << "\n";
+}
