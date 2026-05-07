eb286dfcea433f1530cad5a5f3835b14408e6a6e
Felipe Mota <felipemota465@gmail.com>
2026-05-07 10:46:45 -0300

    Aula 5
    Aula 5

---
+++ Aula 5/soma-das-colunas.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int matriz[3][3];
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> matriz[i][j];
+        }      
+    }
+
+    for(int i = 0; i < 3; i++) {
+        int soma = 0;
+        for(int j = 0; j < 3; j++) {
+            soma += matriz[j][i];
+        }
+        
+        cout << "Coluna " << i << ": " << soma << "\n";
+    }
+}
