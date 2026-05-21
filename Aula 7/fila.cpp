584887b6e43464c4d962a86a0c8c1f1f24a3c35d
Felipe Mota <felipemota465@gmail.com>
2026-05-21 11:06:38 -0300

    Aula 7
    Aula 7

--- Aula 7/fila.cpp
+++ Aula 7/fila.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main(){
+    int n, m, pessoa;
+    cin>>n;
+    vector<int> fila(n);
 
+    for(int i=0; i<n; i++){
+        cin>>fila[i];
+    }
+
+    cin>>m;
+    set<int> sairam;
+
+    for(int i=0; i<m; i++){
+        cin>>pessoa;
+        sairam.insert(pessoa);
+    }
+
+    for(int i=0; i<n; i++){
+        if(sairam.find(fila[i]) == sairam.end()){
+            cout<< fila[i]<< " ";
+        }
+    }
 }
